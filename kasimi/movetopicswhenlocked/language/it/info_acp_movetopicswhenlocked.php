783e078df4efcc8e8335582fc6d7a28c7f0da51d
Mauron <Mauron@users.noreply.github.com>
2015-12-08 11:21:09 +0100

    Preliminary Italian translation for info_acp_movetopicswhenlocked.php

---
+++ kasimi/movetopicswhenlocked/language/it/info_acp_movetopicswhenlocked.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Move Topics When Locked
+ * @copyright (c) 2015 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'MOVE_TOPICS_WHEN_LOCKED'			=> 'Sposta argomenti bloccati', // I suppressed 'automatically' for brevity sake
+	'MOVE_TOPICS_TO'					=> 'Sposta argomenti bloccati in',
+	'LOG_MOVED_LOCKED_TOPIC'			=> '<strong>L’argomento bloccato “%s” è stato automaticamente spostato</strong><br />&raquo; da %s<br />&raquo; a %s',
+));
