2ab703ae0b12726edbdcb7dfeb4c7d8b82cffeae
kasimi <mail@kasimi.net>
2018-05-04 23:40:46 +0200

    Version 1.1.0 drops support for phpBB 3.1

--- kasimi/movetopicswhenlocked/ext.php
+++ kasimi/movetopicswhenlocked/ext.php
@@ -13,14 +13,11 @@ namespace kasimi\movetopicswhenlocked;
 class ext extends \phpbb\extension\base
 {
 	/**
-	 * Requires phpBB 3.1.7-RC1 due to the required event core.mcp_lock_unlock_after
-	 *
 	 * @return bool
 	 * @access public
 	 */
 	public function is_enableable()
 	{
-		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], '3.1.7-RC1', '>=');
+		return phpbb_version_compare(PHPBB_VERSION, '3.2.0', '>=');
 	}
 }
